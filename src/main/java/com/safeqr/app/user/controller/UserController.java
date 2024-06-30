1afbcbe4100b81cf6c408541f9bac30cd1dd19d3
heyethereum <heyethereum@gmail.com>
2024-06-30 13:08:26 +0800

    test health check

--- src/main/java/com/safeqr/app/user/controller/UserController.java
+++ src/main/java/com/safeqr/app/user/controller/UserController.java
@@ -8,15 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/v1")
 public class UserController {
     @Autowired
     UserService userService;
     @GetMapping(value = "/version", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> version() {
-        System.out.println(userService.getUserByEmail());
-        return ResponseEntity.ok("SafeQR v1.0.2");
+    public ResponseEntity<Map<String, String>> version() {
+        //System.out.println(userService.getUserByEmail());
+        return ResponseEntity.ok(Map.of("version","SafeQR v1.0.2"));
     }
 
 }
