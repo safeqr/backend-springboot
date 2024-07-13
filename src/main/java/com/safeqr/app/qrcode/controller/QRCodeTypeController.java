8f570a86057893021d2fe7b0aff710cc360fc4dc
heyethereum <heyethereum@gmail.com>
2024-07-13 23:05:03 +0800

    change to api/v1

--- src/main/java/com/safeqr/app/qrcode/controller/QRCodeTypeController.java
+++ src/main/java/com/safeqr/app/qrcode/controller/QRCodeTypeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/api/qrcodetypes")
+@RequestMapping("/api/v1/qrcodetypes")
 public class QRCodeTypeController {
 
     @Autowired
