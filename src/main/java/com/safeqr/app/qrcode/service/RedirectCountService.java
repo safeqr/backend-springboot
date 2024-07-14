1163648655abe8c16be0e8e4fa6f16fdb2d228f5
heyethereum <heyethereum@gmail.com>
2024-07-14 10:59:29 +0800

    hardcode url to redirect until logic is ready

--- src/main/java/com/safeqr/app/qrcode/service/RedirectCountService.java
+++ src/main/java/com/safeqr/app/qrcode/service/RedirectCountService.java
@@ -6,16 +6,20 @@ import com.safeqr.app.qrcode.dto.RedirectCountResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class RedirectCountService {
+    private static final Logger logger = LoggerFactory.getLogger(RedirectCountService.class);
 
     public Mono<RedirectCountResponse> countRedirects(QRCodePayload payload) {
         String url = payload.getData();
+        logger.info("RedirectCountService: countRedirects: url={}", url);
 
         return WebClient.create()
                 .get()
-                .uri(url)
+                .uri("https://google.com")// replace with url when logic is complete
                 .exchangeToMono(response -> {
                     RedirectCountResponse redirectCountResponse = new RedirectCountResponse();
                     redirectCountResponse.setRedirectCount(response.cookies().size());
